06043133d46ff739490a3fbb409f113e2b9639c4
jjorgeadrian007 <jjorgeadrian007@gmail.com>
2026-03-16 14:23:16 -0500

    Deber semana 14-funciones

---
+++ Semana14.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana14;
+import java.util.Scanner;
+/**
+ *
+ * @author Admin
+ */
+public class Semana14 {
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double b, h;
+        System.out.println("Calculo de area de un rectangulo ");
+        System.out.print("Ingrese la base: ");
+        b=sc.nextDouble();
+        System.out.print("Ingrese la altuta: ");
+        h=sc.nextDouble();
+        double resp=CalculoArea(b,h);
+        System.out.println("El area es: "+resp);
+        
+        
+    }
+    
+    static double CalculoArea(double b, double h){
+        return(b*h);
+    }
+    
+   
+}
